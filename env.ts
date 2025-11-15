d1dbb3146796331d55e5eac4005a55c1b3537834
shaobeichen <shaobeichen@outlook.com>
2025-11-15 15:54:02 +0800

    fix(env): 更新静态资源URL并清理冗余配置
    移除不再使用的key配置，更新图片资源URL尺寸为更合适的220x330
    将token注释更新为已配置状态

--- env.ts
+++ env.ts
@@ -1,8 +1,6 @@
 export default {
     baseUrl: 'https://api.themoviedb.org/3',
-    staticUrl: 'https://image.tmdb.org/t/p/w440_and_h660_face/',
-    // 临时放置，需要配置到仓库 env 变量
+    staticUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/',
+    // 本地模拟，已配置到仓库 env 变量
     token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzA2ZjA3MjY0YTA0ZmI0ZTgyMWFiZTZmOTQ5YjYxMCIsIm5iZiI6MTc2Mjc5NzQ4OC44MzksInN1YiI6IjY5MTIyN2IwNjQ5MDdmYWJlMzMxYjkyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XJppXULkGrAs29zYVdFjoTT56g_GKjpDo0PORU4wEF0',
-    // 临时放置，需要配置到仓库 env 变量
-    key: '1306f07264a04fb4e821abe6f949b610',
 }
